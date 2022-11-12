3793fac187749b13c3cfa26b466114c58c604039
RensSmit <114647755+RensSmit@users.noreply.github.com>
2022-11-12 14:30:44 +0100

    Add files via upload

---
+++ tekstbasedgame2.py
@@ -0,0 +1,349 @@
+def gameover():
+    valid_input = False 
+    while not valid_input: 
+        print("jammer, je bent doodgegaan. wil je het opnieuw proberen?")
+        print("1 ja")
+        print("2 nee")
+        A = input("")
+        if A == "1":
+            print("veel plezier met je run")
+            s1()
+            valid_input = True
+        elif A == "2":
+            print("heel jammer, ik hoop dat je het nog een andre keer een kans geeft")
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")
+
+def einde():
+    valid_input = False 
+    while not valid_input: 
+        print("gefeliciteerd, je hebt mijn game uitgespeeld. je hebt 1 van de goede eindes gevonden.")
+        print("wil je nog een keer spelen?")
+        print("1 ja")
+        print("2 nee")
+        A = input("")
+        if A == "1":
+            print("veel plezier met je run")
+            s1()
+            valid_input = True
+        elif A == "2":
+            print("heel jammer, ik hoop dat je het nog een andre keer een kans geeft")
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")
+
+def s26():
+    print("GEFELICITEERD, je hebt het non simp einde gehaalt. je had kans om dood te gaan dit heb je vermeden goed gedaan.")
+
+def s25():
+    valid_input = False 
+    while not valid_input: 
+        print("Lotte heeft ineens geen intresse meer in je en loopt weg, jij daarin tegen loopt langs de foodbar en ziet pizza liggen.")
+        print("je neemt een goede slize en begint hem naar binnen te schuiven. na het voorledig opeten van 2 stukken ligt er nog 1 stukje pizza hawai.")
+        print("eet je het laaste stuk pizza")
+        print("1 ja")
+        print("2 nee")
+        A = input("")
+        if A == "1":
+            s24()
+            valid_input = True
+        elif A == "2":
+            s26()
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")
+
+def s24():
+        print("na de 3de hap verlik je je in een stukje ananas, in het begin was het gwn hoeste maar je kreeg het er maar niet uit. je was aan het stikken, niemand snapt tot op de dag van vandaag niet dat je het stukje pizaa hawai heb gepakt")
+        print("je bent gestikt in ananas je bent dood.")
+        gameover()
+
+def s23():
+        print("je vrienden vinden je een goote pussy, en nemen je mee naar het meisje.")
+        s8()
+
+def s22():
+    valid_input = False 
+    while not valid_input: 
+        print("je loopt terug naar de vrienden waarmee je aan het praaten was en legt uit wat er net gebeurd is, ze zeggen meteen: ik kan je wingman zijn, jij kan dit, loop er gewoon heen.")
+        print("geef je het een 2de kans?.")
+        print("1 ja")
+        print("2 nee ")
+        A = input("")
+        if A == "1":
+            s8()
+            valid_input = True
+        elif A == "2":
+            s23()
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")
+
+def s21():
+        print("na het fietsen en het terug aangekomen te zijn op het feestje, ")
+        s7()
+
+def s20():
+        print("je start Minecraft op en laat je hardcore survivel wereld in. na een beetje rond rennen in caves vind je deepslate emeralds, ")
+        print("je wilt het gaan minen en hoort achter je ineens. tsssssssss, zonder erop te kunnen reageren ontploft de creeper en ben je dood.")
+        print("je sluit het spel snel af en loopt naar beneden om wat de eten en drinken pakken.")
+        s10()
+
+def s19():
+    valid_input = False 
+    while not valid_input: 
+        print("na het opsmikkelen van je menu en het wachten op de rest van de groep.")
+        print("er gaan een paar mensen naar huis en er gaaner ook een paar terug naar het feestje. ")
+        print("ga je terug naar huis of terug naar het feestje?")
+        print("1 terug naar huis")
+        print("2 terug naar feestje")
+        A = input("")
+        if A == "1":
+            s6()
+            valid_input = True
+        elif A == "2":
+            s21()
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")
+
+ 
+
+def s18():
+    print("je slaat links af bij de groote boom. ")
+    print("na het lopen van 20 min zie je ineens lichtje in het duister, lantaren palen. je hebt de weg terug gevonden naar het dorp.")
+    einde()
+
+
+
+def s17():
+        print("je slaat rechts af bij de boom, na 15 min lopen sta je het randje van een afgrond je zaklamp is leeg en je ziet de val niet aankomen.")
+        print("je valt de afgrond in en je bent nooit meer terug gevonden.")
+        gameover()
+
+
+def s16():
+    valid_input = False 
+    while not valid_input: 
+        print("die zelfde avond gaan jullie in het bos verstoppertje spelen, in het groote bos naast het huisje.")
+        print("jij moet verstoppen en rent zo snel mogelijk een kant op, na 2 uur is je telefoon leeg je zaklamp word steeds minder fel")
+        print("je bent verdwaalt je kan niemand berijken alleen nog maar roepen om hulp, er antwoord niemand.")
+        print("je staat bij een groote boom ga je links of rechts?")
+        print("1 rechts")
+        print("2 links")
+        A = input("")
+        if A == "1":
+            s17()
+            valid_input = True
+        elif A == "2":
+            s18()
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")
+
+def s15():
+    print("je loopt je kamer in en kijkt uit het raam en ziet het mooie uitzicht.")
+    print("je doet de deur open van het balkon en kijkt in de omgeving je ziet bergen en een groot bos. ")
+    print("je gaat op het hekje zitten op het balkon, na 2 min staren naar het uitzicht verlies je je balans en val je naar benden je breekt je nek, je bent dood.")
+    gameover()
+    
+def s14():
+        print("je bent erg moe, je moeder begrijpt het en zegt dat je wel thuis mag blijven, je zwaaid je familie uit als ze wegrijden")
+        print("je loopt naar boven om verder te gaan met gamen, je staat boven en je bedenkt dat je telefoon nog beneden ligt.")
+        print("je loopt de terap af en mist een tré hierdoor rol je de trap af en raak je bewuseloos, je bloed dood.")
+        gameover()
+        
+
+def s13():
+    valid_input = False 
+    while not valid_input: 
+        print("jullie stappen die middag in dxe auto.")
+        print("na een paar uur rijden zijn jullie aangekomen bij het huisje waar jullie dit weekend zullen verblijven, jij mag een akmer kiezen")
+        print("er zijn 2 soorten kamers, een kamer met een balkon en een 2persoons bed. de andere kamer heeft een stapelbed die je moet delen met je zusje.")
+        print("welke kamer kies je?")
+        print("1 kamer met het balkon")
+        print("2 kamer met het stapelbed")
+        A = input("")
+        if A == "1":
+            s15()
+            valid_input = True
+        elif A == "2":
+            s16()
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")
+
+def s12():
+    valid_input = False 
+    while not valid_input: 
+        print("je start valorant op en je start een potje.")
+        print ("4 uur later")
+        print("je zit op een winstreak van 5 het is inmiddels all erg laat dus je sluit je pc af en je gaat slapen.")
+        print("de volgende ochtent bij het ontbijt hoor je dat je een familie weekend heb naar duitsland. ga je mee?")
+        print("1 ja")
+        print("2 nee")
+        A = input("")
+        if A == "1":
+            s13()
+            valid_input = True
+        elif A == "2":
+            s14()
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")
+
+def s11():
+    print("je start fortnite op, je gaat start een potje solo op, je word instand gepoept door 10 jarige kindere die veel de goed zijn.")
+    print("je drukt op alt f4 na 4 keer doodgaan in de eerst 2 min van de game.")
+    s10()
+    
+
+def s10():
+    valid_input = False 
+    while not valid_input: 
+        print("je loopt de trap op naar je kamer drukt je pc aan, op je bureau blad staan een aantal games welke game ga je spelen?. ")
+        print("1 valorant")
+        print("2 fortnite")
+        print("3 minecraft")
+        A = input("")
+        if A == "1":
+            s12()
+            valid_input = True
+        elif A == "2":
+            s11()
+            valid_input = True
+        elif A == "3":
+            s20()
+            valid_input = True
+        else:
+            print("antwoord met 1, 2 of 3.")
+
+def s9():
+    print("aangekomen bij Lotte's huis vraagt ze of je dorst heb je zegt nee omdat je net nog wat gedronken heb je bekijkt de foto's aan de muur en je voelt")
+    print("opeens iets in je rug, iets puntigs. het voelt als een potlood. een mes, je bent vermoord door Lotte.")
+    gameover()
+
+def s8():
+    valid_input = False 
+    while not valid_input: 
+        print("jullie beginen te praaten met het meisje met de naam Lotte, na een kort gesprek steld ze voor om het gesprek voort te zetten bij haar thuis, dit zegt ze met een knip oog.")
+        print("ga je met Lotte mee naar huis?")
+        print("1 ga mee naar huis")
+        print("2 blijf op het feestje")
+        A = input("")
+        if A == "1":
+            s20()
+            valid_input = True
+        elif A == "2":
+            s25()
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")
+
+def s7():
+    valid_input = False 
+    while not valid_input: 
+        print("je hebt dorst gekregen je loopt naar de drankjes bar en zit een meisje staan, je zit te denken of je een praatje moet maken.")
+        print("ga je een praatje maken met het meisje")
+        print("1 loop terug naar je vrienden")
+        print("2 maak een praatje")
+        A = input("")
+        if A == "1":
+            s22()
+            valid_input = True
+        elif A == "2":
+            s8()
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")
+
+def s6():
+    print("onderweg naar huis splits je van de groep na 6 min alleen fietsen zie je iets op de weg lopen je wijkt uit en valt van je fiets.")
+    print("je valt in de sloot en raakt bewusteloos, je verdrinkt.")
+    gameover()
+
+def s5():
+    valid_input = False 
+    while not valid_input: 
+        print("na het opsmikkelen van je mcflurry en het wachten op de rest van de groep.")
+        print("er gaan een paar mensen naar huis en er gaaner ook een paar terug naar het feestje. ")
+        print("ga je terug naar huis of terug naar het feestje")
+        print("1 terug naar huis")
+        print("2 terug naar het feestje")
+        A = input("")
+        if A == "1":
+            s6()
+            valid_input = True
+        elif A == "2":
+            s7()
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")
+
+def s4():
+    valid_input = False 
+    while not valid_input: 
+        print("na het opsmikkelen van je mcflurry en het wachten op de rest van de groep.")
+        print("er gaan een paar mensen naar huis en er gaaner ook een paar terug naar het feestje. ")
+        print("ga je terug naar huis of terug naar het feestje")
+        print("1 terug naar huis")
+        print("2 terug naar feestje")
+        A = input("")
+        if A == "1":
+            s6()
+            valid_input = True
+        elif A == "2":
+            s21()
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")
+
+def s3():
+    valid_input = False 
+    while not valid_input: 
+        print("na het fietsen zijn jij en nog 5 mensen van het feestje aangekomen bij de mcdonalds sta je voor een hele grootte keuze, wat ga je eten?")
+        print("1 je besteld een mcflurry")
+        print("2 je besteld een menu")
+        A = input("")
+        if A == "1":
+            s4()
+            valid_input = True
+        elif A == "2":
+            s19()
+            valid_input = True
+        else:
+            print("answer with 1 or 2.")
+
+def s2():
+    valid_input = False 
+    while not valid_input: 
+        print("het feestje is heel leuk en alles maar jullie beginnen wel honger te krijgen, en er wrod voorgesteld of er mensen mee gaan naar de mcDonalds.")
+        print("1 ga mee naar de mcDonalds")
+        print("2 blijf op het feestje")
+        A = input("")
+        if A == "1":
+            s3()
+            valid_input = True
+        elif A == "2":
+            s7()
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")           
+            
+def s1():
+    valid_input = False 
+    while not valid_input:
+        print("hallo het is een weekend na een lange school week je ligt in bed beetje netflix te kijken, je krijgt een snap van een vried of je naar dit feestje komt ga je heen?")
+        print("1 ga naar het feestje")
+        print("2 blijf thuis")
+        A = input("")
+        if A == "1":
+            s2()
+            valid_input = True
+        elif A == "2":
+            s10()
+            valid_input = True
+        else:
+            print("antwoord met 1 of 2.")           
+s1()
